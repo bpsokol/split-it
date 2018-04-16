4d56b2838d692ea2161362b1d6f2c3f808e14d50
Brian Sokol <bpsokol@crimson.ua.edu>
2018-04-16 11:23:37 -0500

    Hitting enter on keyboard now performs the same action as done button

--- SplitIt/app/src/main/java/project/cs495/splitit/ChangeEmailActivity.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/ChangeEmailActivity.java
@@ -3,6 +3,7 @@ package project.cs495.splitit;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,28 +21,43 @@ public class ChangeEmailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_email);
         setTitle("Change Email");
+
         Button buttonDone = (Button)findViewById(R.id.button_done_email);
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textEmail = (TextView)findViewById(R.id.email_address);
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                String newEmail = textEmail.getText().toString();
-                user.updateEmail(newEmail)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
-                                    finish();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Failed to change Email!", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                updateEmail();
+            }
+        });
+
+        TextView textEmail = (TextView)findViewById(R.id.email_address);
+        textEmail.setOnKeyListener(new View.OnKeyListener(){
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if(keyCode == event.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP){
+                    updateEmail();
+                    return true;
+                }
+                return false;
             }
         });
     }
+
+    private void updateEmail() {
+        TextView textEmail = (TextView)findViewById(R.id.email_address);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String newEmail = textEmail.getText().toString();
+        user.updateEmail(newEmail)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to change Email!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
-
-
--- SplitIt/app/src/main/java/project/cs495/splitit/ChangeUserActivity.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/ChangeUserActivity.java
@@ -4,6 +4,7 @@ package project.cs495.splitit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -27,26 +28,42 @@ public class ChangeUserActivity extends AppCompatActivity {
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textUserID = (TextView)findViewById(R.id.user_id);
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                        .setDisplayName(textUserID.getText().toString())
-                        .build();
-
-                user.updateProfile(profileUpdates)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(),"Success!",Toast.LENGTH_SHORT).show();
-                                    finish();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Failed to change user ID!", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+            updateProfile();
             }
         });
+
+        TextView textUserID = (TextView)findViewById(R.id.user_id);
+        textUserID.setOnKeyListener(new View.OnKeyListener(){
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if(keyCode == event.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP){
+                    updateProfile();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void updateProfile() {
+        TextView textUserID = (TextView)findViewById(R.id.user_id);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(textUserID.getText().toString())
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),"Success!",Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to change user ID!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
