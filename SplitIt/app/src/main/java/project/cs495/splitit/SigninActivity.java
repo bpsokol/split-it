c61823cda91cb1a13c0a5033a89dbe6276056aab
Jacob Tucker <jmtucker6@crimson.ua.edu>
2018-04-19 00:23:58 -0500

    Adds user to firebase on login

--- SplitIt/app/src/main/java/project/cs495/splitit/SigninActivity.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/SigninActivity.java
@@ -4,11 +4,22 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import project.cs495.splitit.models.User;
+import project.cs495.splitit.models.UserBuilder;
 
 public class SigninActivity extends AppCompatActivity {
     private static final String TAG = SigninActivity.class.getSimpleName();
@@ -48,16 +59,37 @@ public class SigninActivity extends AppCompatActivity {
         displayMessage(getString(R.string.unknown_response));
     }
     private boolean isUserLogin(){
-        if(auth.getCurrentUser() != null){
-            return true;
-        }
-        return false;
+        return auth.getCurrentUser() != null;
     }
     private void loginUser(){
+        updateUseInDB();
         Intent loginIntent = new Intent(SigninActivity.this, MainActivity.class);
         startActivity(loginIntent);
         finish();
     }
+
+    private void updateUseInDB() {
+        final FirebaseUser firebaseUser = auth.getCurrentUser();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("users").child(firebaseUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() == null) {
+                    User user = new UserBuilder()
+                            .setUid(firebaseUser.getUid())
+                            .setName(firebaseUser.getDisplayName())
+                            .createUser();
+                    user.commitToDB(database);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Adding user error", databaseError.toException());
+            }
+        });
+    }
+
     private void displayMessage(String message){
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
--- SplitIt/app/src/main/java/project/cs495/splitit/models/User.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/models/User.java
@@ -1,16 +1,29 @@
 package project.cs495.splitit.models;
 
-public class User {
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Map;
+
+public class User implements EntityInterface {
     private String uid;
     private String name;
+    private Map<String, Boolean> groups;
+    private Map<String, Boolean> userReceipts;
 
     public User() {
 
     }
 
-    public User(String uid, String name) {
+    User(String uid, String name, Map<String, Boolean> groups, Map<String, Boolean> userReceipts) {
         this.uid = uid;
         this.name = name;
+        this.groups = groups;
+        this.userReceipts = userReceipts;
+    }
+
+    @Override
+    public void commitToDB(DatabaseReference mDatabase) {
+        mDatabase.child("users").child(this.uid).setValue(this);
     }
 
     public String getUid() {
