ce9f5fc78bdbbf50ee74ec8dea4fa17f8c865a85
tr1503 <hanlinye6@gmail.com>
2018-04-19 23:44:46 -0500

    Make new user can be saved to database

--- SplitIt/app/src/main/java/project/cs495/splitit/SigninActivity.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/SigninActivity.java
@@ -9,20 +9,45 @@ import android.widget.Button;
 import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import project.cs495.splitit.models.User;
 
 public class SigninActivity extends AppCompatActivity {
     private static final String TAG = SigninActivity.class.getSimpleName();
     private static final String PATH_TOS = "";
     private Button loginButton;
     private FirebaseAuth auth;
+    private DatabaseReference database;
     private static final int RC_SIGN_IN = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
         if(isUserLogin()){loginUser();}
         setContentView(R.layout.activity_signin);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()) {
+                    User user = new User(auth.getCurrentUser().getUid(),auth.getCurrentUser().getDisplayName());
+                    database.child("users").child(user.getUid()).setValue(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        database.child("users").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent(eventListener);
         loginButton = (Button)findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
