8dc65d9081df7548f0efdb9388b7ae4017f2b66e
Logan <laskinner@crimson.ua.edu>
2018-04-20 21:04:19 -0500

    User payments page
    Still a work in progress... Some errors

--- SplitIt/app/src/main/java/project/cs495/splitit/UserPaymentActivity.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/UserPaymentActivity.java
@@ -1,10 +1,19 @@
 package project.cs495.splitit;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -12,19 +21,79 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import org.json.JSONArray;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserPaymentActivity extends AppCompatActivity {
 
-    DatabaseReference database;
-    ListView listview;
-    ArrayList<String> list = new ArrayList<>();
+    public class Bill {
+        private String name = "";
+        private String email = "";
+        private String amount = "";
+
+        public Bill(String name, String email, String amount) {
+            this.name = name;
+            this.email = email;
+            this.amount = amount;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setAmount(String amount) {
+            this.amount = amount;
+        }
+
+        public String getAmount() {
+            return amount;
+        }
+    }
+
+    public class BillAdapter extends ArrayAdapter<Bill> {
+
+        public BillAdapter( Context context, ArrayList<Bill> list) {
+            super(context, 0, list);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View listItem = convertView;
+            if(listItem == null)
+                listItem = LayoutInflater.from(mContext).inflate(R.layout.item_view,parent,false);
+
+            Bill currentBill = billList.get(position);
+
+            TextView name = (TextView) listItem.findViewById(R.id.manager_name);
+            name.setText(currentBill.getName());
+
+            TextView email = (TextView) listItem.findViewById(R.id.manager_email);
+            email.setText(currentBill.getEmail());
+
+            TextView amount = (TextView) listItem.findViewById(R.id.amount_owed);
+            amount.setText(currentBill.getAmount());
+
+            return listItem;
+        }
+    }
+
+    private DatabaseReference database;
+    private ListView listView;
+    private BillAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +101,11 @@ public class UserPaymentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_payment);
         setTitle("Bills");
 
-        listview = (ListView) findViewById(R.id.bill_listview);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
-        listview.setAdapter(adapter);
+        listView = (ListView) findViewById(R.id.bill_listview);
+        final ArrayList<Bill> billList = new ArrayList<>();
+        adapter = new BillAdapter(this,billList);
+        listView.setAdapter(adapter);
+
         database = FirebaseDatabase
                 .getInstance()
                 .getReference()
@@ -45,7 +116,9 @@ public class UserPaymentActivity extends AppCompatActivity {
         database.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                list.add("$ " + dataSnapshot.child("cost").getValue(String.class));
+                billList.add(new Bill(dataSnapshot.child("managerName").getValue(String.class)
+                        ,dataSnapshot.child("managerEmail").getValue(String.class)
+                        ,dataSnapshot.child("cost").getValue(String.class)));
 
                 adapter.notifyDataSetChanged();
             }
@@ -57,7 +130,8 @@ public class UserPaymentActivity extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-                list.remove(dataSnapshot.getValue(String.class));
+                billList.remove(dataSnapshot.getValue(String.class));
+
                 adapter.notifyDataSetChanged();
             }
 
@@ -72,6 +146,4 @@ public class UserPaymentActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
