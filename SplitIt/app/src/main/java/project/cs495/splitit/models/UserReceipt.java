04c21fb0d2fd5460b2aef04a5b44edf2122fec4b
Jacob Tucker <jmtucker6@crimson.ua.edu>
2018-04-22 22:32:38 -0500

    Adds ability to create a user receipt from receipt view

---
+++ SplitIt/app/src/main/java/project/cs495/splitit/models/UserReceipt.java
@@ -0,0 +1,31 @@
+package project.cs495.splitit.models;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Map;
+
+public class UserReceipt extends Receipt {
+    private String userId;
+
+    public UserReceipt() {
+        super();
+    }
+
+    public UserReceipt(String receiptId, String creator, String vendor, String datePurchased, float price, Map<String, Boolean> items, String groupId, String userId) {
+        super(receiptId, creator, vendor, datePurchased, price, items, groupId);
+        this.userId = userId;
+    }
+
+    @Override
+    public void commitToDB(DatabaseReference mDatabase) {
+        mDatabase.child("users").child(userId).child("receipts").child(receiptId).setValue(this);
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
