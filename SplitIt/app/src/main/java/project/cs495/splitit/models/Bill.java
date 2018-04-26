570c63064b1368e8436ac5c2a748e94a6be5845c
Jacob Tucker <jmtucker6@crimson.ua.edu>
2018-04-26 13:36:33 -0500

    WIP

--- SplitIt/app/src/main/java/project/cs495/splitit/models/Bill.java
+++ SplitIt/app/src/main/java/project/cs495/splitit/models/Bill.java
@@ -6,11 +6,23 @@ public class Bill {
     private String name = "";
     private String email = "";
     private String amount = "";
+    private String uid;
 
     public Bill(String name, String email, String amount) {
         this.name = name;
         this.email = email;
         this.amount = amount;
+    }
+
+    public Bill(String name, String email, String amount, String uid) {
+        this.name = name;
+        this.email = email;
+        this.amount = amount;
+        this.uid = uid;
+    }
+
+    public Bill() {
+
     }
 
     public void setName(String name) {
@@ -37,4 +49,17 @@ public class Bill {
         return amount;
     }
 
+    public float getAmountAsFloat() { return Float.parseFloat(amount); }
+
+    public void setAmountFromFloat(Float amount) {
+        this.amount = String.format("%.2f", amount);
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
 }
